a0be12ad1e134c6f8070f62878ecadaef3545d29
Gordon Smith <gordon.smith@lexisnexis.com>
2015-09-10 13:55:04 +0100

    Gen Dash
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- src/common/Widget.js
+++ src/common/Widget.js
@@ -30,6 +30,7 @@
         this._pos = { x: 0, y: 0 };
         this._size = { width: 0, height: 0 };
         this._scale = 1;
+        this._visible = true;
 
         for (var key in this) {
             if (key.indexOf("__meta_") === 0) {
@@ -688,6 +689,10 @@
     //  Render  ---
     Widget.prototype.render = function (callback) {
         callback = callback || function () { };
+        if (!this.visible()) {
+            callback(this);
+            return this;
+        }
         if (this._parentElement) {
             if (!this._tag)
                 throw "No DOM tag specified";
